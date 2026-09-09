78297cfe882e02fed25a7df8f0dd4b8d0ac0614f
KlebberCandido <klebbercandido2610@gmail.com>
2026-09-09 20:40:03 -0300

    maior que

--- main.c
+++ main.c
@@ -5,8 +5,10 @@ int main(){
 int a = 5;
 int b = 4;
 
-printf("o maior número é: %d\n", (a > b ) ? a : b);
-
+printf("o maior número é: %d\n", (a = b ));
+printf("a subtração dos dois numeros e : %d\n", (a - b));
+printf("a mutiplicação dos dois numeros e: %d\n",(a * b));
+printf("a divisão dos dois numeros e: %.2f\n", (float)a / b); 
 
 
 return 0; 
