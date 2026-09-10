08a3fc591f61a0a274d9bd6791fcbcb6810b3e50
KlebberCandido <klebbercandido2610@gmail.com>
2026-09-09 21:34:55 -0300

    solução com acento

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
+#include<locale.h>
 
 int main(){
+
+setlocale(LC_ALL, "pt_BR.UTF-8");
 
 int a = 5;
 int b = 4;
 
-printf("o maior número é: %d\n", (a = b ));
+printf("o maior número é: %d\n", (a + b ));
 printf("a subtração dos dois numeros e : %d\n", (a - b));
 printf("a mutiplicação dos dois numeros e: %d\n",(a * b));
-printf("a divisão dos dois numeros e: %.2f\n", (float)a / b); 
+printf("a divisão dos dois numeros e: %d\n", (a / b)); 
 
 
 return 0; 
