754ba637d199fb619c43f905147d305c89cddacd
KlebberCandido <klebbercandido2610@gmail.com>
2026-09-11 19:37:34 -0300

    Antecessor

--- main.c
+++ main.c
@@ -2,27 +2,20 @@
 #include<locale.h>
 #include<stdbool.h>
 
-bool ehPar(int numero){
-    return(numero % 2 == 0);
-}
 
 int main(){
 
 setlocale(LC_ALL, "pt_BR.UTF-8");
 
+int n;
 
-bool resultado1 = ehPar(4);
-bool resultado2 = ehPar(7);
-bool resultado3 = ehPar(8);
-bool resultado4 = ehPar(14);
-bool resultado5 = ehPar(100);
+printf("DIgite um numero: ");
+scanf("%d", &n);
+
+printf("Antecessor: %d\n", n- 1);
+printf("Sucessor; %d\n", n+ 1);
 
 
-printf("4 e par? %d\n", resultado1);
-printf("7 e par ? %d\n", resultado2);
-printf("8 e par? %d\n", resultado3);
-printf("14 e par? %d\n", resultado4);
-printf("100 e par? %d\n", resultado5);
 
 
 
