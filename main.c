0fd862968eb1c80379835a708afe1d617e099a66
KlebberCandido <klebbercandido2610@gmail.com>
2026-09-11 21:45:09 -0300

    segundos, horas

--- main.c
+++ main.c
@@ -7,13 +7,16 @@ int main(){
 
 setlocale(LC_ALL, "pt_BR.UTF-8");
 
-float celsius, fahrenheit;
+int segundos, horas, minutos, resto;
+printf("Digite a quantidade de segundos: ");
+scanf("%d", &segundos);
 
-printf("Digite a temperatura em Celsius: ");
-scanf("%f", &celsius);
+horas = segundos / 3600;
+minutos = (segundos % 3600) / 60;
+resto = segundos % 60;
 
-fahrenheit = (celsius * 9.0 / 5.0) + 32;
-printf("Fahrenheit: %.2f\n", fahrenheit);
+printf("%d horas, %d minutos  e %d segundos\n", horas, minutos, resto);
+
 
 
     
