a8898a9e2333aa1f94d784ff8b773ba775000995
KlebberCandido <klebbercandido2610@gmail.com>
2026-09-14 20:53:10 -0300

    teste Maior Menor aprimorado

--- main.c
+++ main.c
@@ -7,15 +7,19 @@ int main(){
 
 setlocale(LC_ALL, "pt_BR.UTF-8");
 
-int a, b, c, maior;
+int a, b, c, d, e, maior;
 
-printf("Digite tres, numeros: ");
-scanf("%d %d %d", &a, &b, &c);
+printf("Digite cinco, numeros: ");
+scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
 
 maior = a;
 
+
 if(b > maior) maior = b;
 if(c > maior) maior = c;
+if(d > maior) maior = d;
+if(e > maior) maior = e;
+
 
 printf("Maior: %d", maior);
 
