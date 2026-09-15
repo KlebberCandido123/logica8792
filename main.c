a25d319de99daf6fd016a7897f50737f76948922
KlebberCandido <klebbercandido2610@gmail.com>
2026-09-14 21:43:10 -0300

    Numeros iguais e diferentes

--- main.c
+++ main.c
@@ -7,49 +7,16 @@ int main(){
 
 setlocale(LC_ALL, "pt_BR.UTF-8");
 
-int a, b, c, d, e, menor, maior;
+int a, b;
 
-printf("Digite um numero para a:");
-scanf("%d", &a);
+printf("digiite dois numeros:");
+scanf("%d %d", &a, &b);
 
-printf("Digite um numero para b:");
-scanf("%d", &b);
-
-printf("Digite um numero para c:");
-scanf("%d", &c);
-
-printf("Digite um numero para d:");
-scanf("%d", &d);
-
-printf("Digite um numero para e:");
-scanf("%d", &e);
-
-
-
-menor = a;
-
-
-if(b < menor) menor = b;
-if(c < menor) menor = c;
-if(d < menor) menor = d;
-if(e < menor) menor = e;
-
-
-
-
-
-maior = a;
-
-
-if(b > maior) maior = b;
-if(c > maior) maior = c;
-if(d > maior) maior = d;
-if(e > maior) maior = e;
-
-
-
-printf("Maior: %d\n", maior);
-printf("Menor: %d\n", menor);
+if(a == b){
+    printf("Os números são iguais!\n");
+}else{
+    printf("Os numeros são diferentes!\n");
+}
 
 
 
