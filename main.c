c43c6378ae03d561b85427a3ee29c2c627147a3a
KlebberCandido <klebbercandido2610@gmail.com>
2026-09-14 21:06:52 -0300

    Dgite um valor para a,b,c,d,e,

--- main.c
+++ main.c
@@ -9,8 +9,22 @@ setlocale(LC_ALL, "pt_BR.UTF-8");
 
 int a, b, c, d, e, maior;
 
-printf("Digite cinco, numeros: ");
-scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+printf("Digite um numero para a:");
+scanf("%d", &a);
+
+printf("Digite um numero para b:");
+scanf("%d", &b);
+
+printf("Digite um numero para c:");
+scanf("%d", &c);
+
+printf("Digite um numero para d:");
+scanf("%d", &d);
+
+printf("Digite um numero para e:");
+scanf("%d", &e);
+
+
 
 maior = a;
 
