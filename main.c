0f456f3dacc34cb42b2d14cf0638c8e6fe515afa
KlebberCandido <klebbercandido2610@gmail.com>
2026-09-16 20:52:16 -0300

    switch

--- main.c
+++ main.c
@@ -7,29 +7,49 @@ int main(){
 
 setlocale(LC_ALL, "pt_BR.UTF-8");
 
-int  diaDaSemana;
+int  dia;
 
 printf("Digite um numero de (1 a 7): ");
-scanf("%d", &diaDaSemana);
+scanf("%d", &dia);
 
-if(diaDaSemana == 1){
-    printf("DOmingo!");
-
-}else if(diaDaSemana == 2){
-    printf("Segunda-feira");
-}else if(diaDaSemana == 3){
-    printf("Terça-feira");
-}else if(diaDaSemana == 4){
+switch (dia){
+    case 1:
+       printf("Domingo!");
+       break;
+    case 2:
+        printf("Segunda-feira");
+        break;
+    case 3:
+        printf("Terça-feira");
+        break;
+    case 4:
     printf("Quarta-feira");
-}else if(diaDaSemana == 5){
-    printf("Quinta-feira");
-}else if(diaDaSemana == 6){
-    printf("Sexta-feira");
-}else if (diaDaSemana == 7){
-    printf("Sabado!");
-}else{
-    printf("valor invalido!");
+         break;
+    case 5: 
+         printf("Quinta-feira");
+           break;
+    case 6:
+         printf("Sexta-feira");
+         break;
+    case 7:
+        printf("Sabado");
+        break;
+       default:
+       printf("Numero invalido!!");
+       break;  
+
+
+
+
+
+
+
+
+
+
 }
+
+
 
 
 
