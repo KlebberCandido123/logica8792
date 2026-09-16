02116e97cbb001a4b0ebde5d3c402e39832c4f5c
KlebberCandido <klebbercandido2610@gmail.com>
2026-09-16 19:55:22 -0300

    Aprovado,Reprovado

--- main.c
+++ main.c
@@ -7,20 +7,35 @@ int main(){
 
 setlocale(LC_ALL, "pt_BR.UTF-8");
 
-int idade;
-int ehEstudante;
+int n1, n2, n3;
+float media;
+float freq;
 
-printf("Qual sua idade?");
-scanf("%d", &idade);
-printf("\nVoce é estudante (0 - Não | 1 - Sim):");
-scanf("%d", &ehEstudante);
 
-if((idade >= 60) && (ehEstudante == 1)){
-    printf("parabens voce tem direito a desconto!");
+printf("\nDigite a nota 1: ");
+scanf("%d", &n1);
+
+
+
+printf("\nDigite a nota 2: ");
+scanf("%d", &n2);
+
+
+printf("\nDigite a nota 3: ");
+scanf("%d", &n3);
+
+media = (n1 + n2 + n3) / 3;
+
+printf("Digite a frequencia (em numeros apenas): ");
+scanf("%f", &freq);
+
+if(media >= 7 && freq >= 75){
+    printf("\nAprovado com suceso!");
 
 }else{
-    printf("infelizmente voce não tera desconto!");
+    printf("\nReprovado!");
 }
+
 
 
 
