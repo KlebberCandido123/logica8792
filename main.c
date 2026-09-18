289867c6cd0d31c5a4dba36690e520e7e44da99d
KlebberCandido <klebbercandido2610@gmail.com>
2026-09-18 20:15:49 -0300

    estações do ano

--- main.c
+++ main.c
@@ -7,59 +7,46 @@ int main(){
 
 setlocale(LC_ALL, "pt_BR.UTF-8");
 
-int opcao;
+int mes;
 
-printf("--- sistema de pedagio---\n");
-printf("1 - moto\n");
-printf("2 - carro passeio\n");
-printf("3 - Caminhão\n");
-printf("4- onibus\n");
-scanf("%d", &opcao);
+printf("Digite um número de 1 a 12 meses");
+scanf("%d", &mes);
 
-switch (opcao)
-{
- case 1:
-    printf("categoria: moto | tarifa: R$ 5,00\n");
-    break;
-case 2:
-    printf("categoria: carro passeio | tarifa: R$ 10,00\n");
-    break;
-case 3:
-    printf("categoria: caminhão | Tarifa: R$25,00\n");
-    break;
-case 4:
-    printf("Categoria: onibus | Tarifa: R$ 30,00\n");
+switch (mes){
+    case 12:
+    case 1 :
+    case 2 :
+         printf("verão\n");
+         break; 
+    
+    case 3 :
+    case 4 :
+    case 5 :
+         printf("Outono\n");
+         break;
+    
+    case 6:
+    case 7:
+    case 8:
+        printf("inverno\n");
+        break;
+
+    case 9:
+    case 10:
+    case 11:
+    printf("primavera\n");
     default:
-    printf("Erro: Categoria não cadastrada no sistema.\n");
+    printf("numero invalido");
     break;
+
+
 }
 
 
 
 
 
-
          
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
 
 
 
