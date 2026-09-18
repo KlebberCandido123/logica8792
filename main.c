b54f27d4b5a885cbad81c536a679283c24816b95
KlebberCandido <klebbercandido2610@gmail.com>
2026-09-18 20:56:47 -0300

    Vogal/Consoante

--- main.c
+++ main.c
@@ -7,40 +7,19 @@ int main(){
 
 setlocale(LC_ALL, "pt_BR.UTF-8");
 
-int mes;
+char letra;
+printf("Digite uma letra: ");
+scanf("%c", &letra);
 
-printf("Digite um número de 1 a 12 meses");
-scanf("%d", &mes);
-
-switch (mes){
-    case 12:
-    case 1 :
-    case 2 :
-         printf("verão\n");
-         break; 
-    
-    case 3 :
-    case 4 :
-    case 5 :
-         printf("Outono\n");
-         break;
-    
-    case 6:
-    case 7:
-    case 8:
-        printf("inverno\n");
-        break;
-
-    case 9:
-    case 10:
-    case 11:
-    printf("primavera\n");
-    default:
-    printf("numero invalido");
-    break;
+if(letra == 'a' || letra =='e' ||letra == 'i' || letra == 'o'  || letra == 'u'||
+    letra == 'A' || letra == 'E' || letra == 'I' || letra == 'o' || letra == 'U'){
+        printf("vogal\n");
+    }else{
+        printf("consoante\n");
+    }
 
 
-}
+
 
 
 
