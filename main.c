2a22bcdc28b073a19798dfd943349e04c49aafa9
KlebberCandido <klebbercandido2610@gmail.com>
2026-09-21 21:53:40 -0300

    sizeof

--- main.c
+++ main.c
@@ -7,13 +7,13 @@
 int main(){
 
 setlocale(LC_ALL, "pt_BR.UTF-8");
-int numeros[3];
 
-numeros[0] = 10;
-numeros[1] = 20;
-numeros[2] = 30;
 
-printf("%d", numeros[2]);
+int numeros[] = {10, 20, 30};
+
+printf("%zu", sizeof(numeros));
+
+
 
 
 
