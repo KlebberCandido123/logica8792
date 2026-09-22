ee5a2b85757eca7bb502218ac3ddd775f445b8d9
KlebberCandido <klebbercandido2610@gmail.com>
2026-09-21 22:00:07 -0300

    22:00

--- main.c
+++ main.c
@@ -11,10 +11,10 @@ setlocale(LC_ALL, "pt_BR.UTF-8");
 
 int numeros[] = {10, 20, 30};
 
-printf("%zu", sizeof(numeros));
+int lenght = sizeof(numeros) / sizeof(numeros[0]);
 
 
-
+printf("%d", lenght);
 
 
 
