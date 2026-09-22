2b719a0e73f0189d3ac1db30ee3c70fa293ce8cc
KlebberCandido <klebbercandido2610@gmail.com>
2026-09-21 21:33:46 -0300

    for5

--- main.c
+++ main.c
@@ -8,10 +8,23 @@ int main(){
 
 setlocale(LC_ALL, "pt_BR.UTF-8");
 
-for(int i = o; i <= 10; i = i + 2){
-    printf("%d\n", i);
+int soma = 0;
+int i;
+
+for(i = 1; i <= 5; i++){
+
+//0 + 1
+//1 + 2
+//3+ 3
+//6+ 4
+//10+ 5
+
+
+
+    soma = soma + i;
 }
 
+printf("soma e %d\n", soma);
 
 
 
