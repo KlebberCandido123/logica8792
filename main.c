98c10667aa24de756583d9337b6c0eb6a297361e
KlebberCandido <klebbercandido2610@gmail.com>
2026-09-22 20:53:28 -0300

    função calculos

--- main.c
+++ main.c
@@ -4,8 +4,24 @@
 #include<string.h>
 
 
-void saudar(const char *nome){
-    printf("ola %s\n", nome);
+int somar(int a, int b){
+    return a + b;
+}
+
+int subtrair(int a, int b){
+    return a - b;
+}
+
+int mutiplicar(int a, int b){
+    return a * b;
+}
+
+int dividir (int a, int b){
+    if( b <= 0){
+        printf("ele precisa ser maior que 0!");
+    }else{
+        return a / b; 
+    }
 }
 
 int main(){
@@ -13,12 +29,21 @@ int main(){
 setlocale(LC_ALL, "pt_BR.UTF-8");
 
 
-saudar("Klebber");
-saudar("vinicius");
+int resultado = somar(5, 3);
+printf("o resultado da soma e: %d\n", resultado);
 
 
 
-         
+int resultado1 = subtrair(5, 3);
+printf("o resultado e: %d\n", resultado1);
+
+
+int resultado2 = mutiplicar(5, 3);
+printf("o resultado e: %d\n", resultado2);
+
+
+int resultado3 = dividir(5, 3);
+printf("o resultado e: %d\n", resultado3);
 
 
 
