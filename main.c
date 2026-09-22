dcaf8490298de418133ba9dc253dcd6a77660f8e
KlebberCandido <klebbercandido2610@gmail.com>
2026-09-22 20:59:06 -0300

    20:58 22/09

--- main.c
+++ main.c
@@ -4,46 +4,17 @@
 #include<string.h>
 
 
-int somar(int a, int b){
-    return a + b;
-}
 
-int subtrair(int a, int b){
-    return a - b;
-}
-
-int mutiplicar(int a, int b){
-    return a * b;
-}
-
-int dividir (int a, int b){
-    if( b <= 0){
-        printf("ele precisa ser maior que 0!");
-    }else{
-        return a / b; 
-    }
-}
 
 int main(){
 
 setlocale(LC_ALL, "pt_BR.UTF-8");
 
+int num[5] = {1, 2, 3, 4, 5};
 
-int resultado = somar(5, 3);
-printf("o resultado da soma e: %d\n", resultado);
+printf("%d", num[0]);
+printf("%d", num[2]);
 
-
-
-int resultado1 = subtrair(5, 3);
-printf("o resultado e: %d\n", resultado1);
-
-
-int resultado2 = mutiplicar(5, 3);
-printf("o resultado e: %d\n", resultado2);
-
-
-int resultado3 = dividir(5, 3);
-printf("o resultado e: %d\n", resultado3);
 
 
 
