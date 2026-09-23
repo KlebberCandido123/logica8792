538fbbef5a9b01c8627d89457a7c5daf2d97b5ac
KlebberCandido <klebbercandido2610@gmail.com>
2026-09-22 21:12:48 -0300

    for

--- main.c
+++ main.c
@@ -10,10 +10,13 @@ int main(){
 
 setlocale(LC_ALL, "pt_BR.UTF-8");
 
+
 int num[5] = {1, 2, 3, 4, 5};
 
-printf("%d", num[0]);
-printf("%d", num[2]);
+for(int i = 0; i < 5; i++){
+   printf("%d\n", num[i]);
+}
+
 
 
 
