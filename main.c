8fce3312cf6a4711bb77f2f173b381044c79fddd
KlebberCandido <klebbercandido2610@gmail.com>
2026-09-23 20:03:46 -0300

    Tabuada função

--- main.c
+++ main.c
@@ -5,16 +5,11 @@
 #include<math.h>
 
 
-void fibonacci(int termos){
-    int a = 0, b = 1, c;
-    printf("Sequecia de fibonacci (%d termo): \n");
-    for(int i = 1; i <= termos; i++){
-        printf("%d\n", a);
-        c = a + b;
-        a = b;
-        b = c;
+void tabuada (int n){
+    printf("tabuada do %d\n", n);
+    for(int i = 1; i <= 10; i++){
+        printf("%d x %d = %d\n", n, i, n * i);
     }
-    printf("\n");
 }
 
 
@@ -23,11 +18,7 @@ int main(){
  setlocale(LC_ALL, "pt_BR.UTF-8");
 
 
-
-
-
-fibonacci(10);
-
+tabuada(7);
 
 return 0; 
 
