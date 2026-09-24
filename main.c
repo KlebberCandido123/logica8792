c6c7a9af76303e7332ea635d94982079247f022e
KlebberCandido <klebbercandido2610@gmail.com>
2026-09-23 21:35:40 -0300

    Vogais

--- main.c
+++ main.c
@@ -4,18 +4,29 @@
 #include<string.h>
 #include<math.h>
 
-void linha(int tamanho){
-    for(int i = 0; i < tamanho; i++){
-        printf("*");
+
+void contarVogais(char palavra[]){
+    int contador = 0;
+    for(int i = 0; i <strlen(palavra); i++){
+        char c = palavra[i];
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+        || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
+            contador++;
+        }
     }
-    printf("\n");
+    printf("A palavra '%s' tem %d vogais.\n", palavra, contador);
 }
 
 int main(){
 
  setlocale(LC_ALL, "pt_BR.UTF-8");
 
-linha(2000);
+char p[20];
+
+printf("Digite uma palavra!: ");
+scanf("%s", p);
+
+contarVogais(p);
 
 
 
