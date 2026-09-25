9d4c0dec239fa7ac54dbeef64f127e1610c0949a
KlebberCandido <klebbercandido2610@gmail.com>
2026-09-24 21:43:37 -0300

    tabuada melhor

--- main.c
+++ main.c
@@ -10,14 +10,21 @@ int main(){
 
  setlocale(LC_ALL, "pt_BR.UTF-8");
 
-int n, tabuada;
+int n;
+int resultado;
 
-printf("Digite um numero: ");
-scanf("%d", &n);
+printf("Digite um numero para tabuada: ");
+    resultado = scanf("%d", &n);
 
-for(int i = 1; i<= 10; i++){
-    printf("%d\n", n  * i);
-}
+    if(resultado != 1){
+         printf("entrada invalida! por favor, digite apenas numeros!");
+        return 1;
+    }
+       
+    for(int i = 1; i <= 10; i++){
+        int resposta = n * i;
+        printf("o resultado de %d x %d e: %d\n", n, i, resposta);
+    }
 
 
 
